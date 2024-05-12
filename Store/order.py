dc04084a48556aa28ea170e8e9a4b5aad0978b8c
shachafemanoel <shachaf331@gmail.com>
2024-05-12 18:08:36 +0300

    סידור באגים

--- Store/order.py
+++ Store/order.py
@@ -30,7 +30,7 @@ class Order:
             return f"{self.total_amount}₪ILS"
 
     def payments(self):
-        return f"{self.total_amount}ILS or {round(self.total_amount/12,2)}/ 12/mo. for 12 mo.*"
+        return f"{self.total_amount}ILS or {round(self.total_amount/12,2)}₪ILS 12/mo. for 12 mo.*"
     def pay_order(self,payme):
         self.payment = payme
         self.status = "Processing"
