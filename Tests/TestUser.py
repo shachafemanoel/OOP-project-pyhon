fb154454cfe9184c960731d6c0eb0064f3c47dae
NirelJano <nireljano@gmail.com>
2024-05-15 00:00:30 +0800

    טסט ליוזר

--- Tests/TestUser.py
+++ Tests/TestUser.py
@@ -1,24 +1,34 @@
 import unittest
-
 from Store.user import User
 
-
 class TestUser(unittest.TestCase):
-    def test_constructor(self):
-        user = User(123456789, "Nirel", 123456789)
-        self.assertEqual(user.user_id, 123456789)
-        self.assertEqual(user.user_full_name, 'Nirel')
-        self.assertEqual(user.password, 123456789)
+    def setUp(self):
+        self.user = User("1234", "Nirel Jano", '1234', 0, 'Haifa', 'Credit Card')
+    def test_initialization(self):
+        self.assertEqual(self.user.user_id, "1234")
+        self.assertEqual(self.user.user_full_name, "Nirel Jano")
+        self.assertEqual(self.user.password, '1234')
+        self.assertEqual(self.user.online, 0)
+        self.assertEqual(self.user.address, 'Haifa')
+        self.assertEqual(self.user.payment, 'Credit Card')
 
     def test_login(self):
-        user = User('123456789', "Nirel", '123456789')
-        user.login("123456789")
-        self.assertEqual(user.online, 1)
+        self.assertFalse(self.user.login("12345"))
+        self.assertEqual(self.user.online, 0)
+        self.assertTrue(self.user.login("1234"))
+        self.assertEqual(self.user.online, 1)
 
-        user = User('123456789', "Nirel", '123456789')
-        user.login("123456782")
-        self.assertEqual(user.online, 0)
+    def test_logout(self):
+        self.user.online = 1
+        self.assertEqual(self.user.logout(), 'Logged out successfully.')
+        self.assertEqual(self.user.online, 0)
 
+    def test_change_user_password(self):
+        self.assertTrue(self.user.change_user_password("1234567"))
+        self.assertEqual(self.user.password, "1234567")
+
+    def test_str(self):
+        self.assertEqual(str(self.user), "User: Nirel Jano\nID: 1234")
 
 if __name__ == '__main__':
     unittest.main()
